81678dce12b751d6c4b06405cca82fd89005b4da
Anjali <anjalisonalkar1374@gmail.com>
2025-12-16 13:03:25 +0530

    Assignment 2

--- Assignment 2/q1.c
+++ Assignment 2/q1.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdint.h>
 
-int main(void)
-{
-    uint8_t status = 0x08;
+int main(void) {
+uint8_t reg = 0x2A;
 
-    if (status & (1 << 3))
-    {
-        printf("Bit 3 is SET\n");
-    }
-    else
-    {
-        printf("Bit 3 is CLEAR\n");
-    }
+// to Set bit 4
+reg = (1 <<4);
+printf(" set bit 4: 0x%02X (0011 1010)\n", reg);
 
-    return 0;
+// to Clear bit 1
+reg &= ~(1 << 1);
+printf(" clear bit 1: 0x%02X (0011 1000) \n", reg);
+
+// to Toggle bit 5
+reg ^= (1 << 5);
+printf(" toggle bit 5: 0x%02X (0001 1000)\n", reg);
+return 0;
+
 }
