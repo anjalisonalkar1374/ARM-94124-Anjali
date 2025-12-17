26ef93bde0372988c3a3efefa3391965157abd49
Anjali <anjalisonalkar1374@gmail.com>
2025-12-17 17:05:12 +0530

    Assignment 2

--- Assignment 2/q1.c
+++ Assignment 2/q1.c
@@ -1,20 +1,17 @@
 #include<stdio.h>
-#include <stdint.h>
-
-int main(void) {
-uint8_t reg = 0x2A;
-
-// to Set bit 4
-reg = (1 <<4);
-printf(" set bit 4: 0x%02X (0011 1010)\n", reg);
-
-// to Clear bit 1
-reg &= ~(1 << 1);
-printf(" clear bit 1: 0x%02X (0011 1000) \n", reg);
-
-// to Toggle bit 5
-reg ^= (1 << 5);
-printf(" toggle bit 5: 0x%02X (0001 1000)\n", reg);
-return 0;
-
+int main(){
+    unsigned int r = 0x2A;
+    printf("Original value : %8b\nhex value : %x",r,r);
+    unsigned int a = 0x80;
+    a=a>>3;
+    r=r|a;
+    printf("\nSet bit 4 : %8b\nhex value : %x",r,r);
+    a=0x80;
+    a=a>>6;
+    r=r&~(a);
+    printf("\nClear bit 1 : %8b\nhex value : %x",r,r);
+    a=0x80;
+    a=a>>2;
+    r=r^a;
+    printf("\nToggle bit 5 : %8b\nhex value : %x",r,r);
 }
